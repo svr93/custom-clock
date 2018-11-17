d3c4849070dc6c1d2e99a44919597ba4b46591fb
shamsheev <svr93@i.ua>
2018-11-17 17:33:32 +0300

    create minimal working example

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,3 +1,14 @@
 export class CustomClockComponent extends HTMLElement {
   public $highlightedHourList: number[] = []
+
+  constructor() {
+    super()
+
+    const template = document.createElement('template')
+    template.innerHTML = `
+      <div>CustomClockComponent</div>
+    `
+
+    this.attachShadow({ mode: 'closed' }).appendChild(template.content)
+  }
 }
