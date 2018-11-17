5989a3a957642ce2aa47a6a0c20319a06ed32f65
shamsheev <svr93@i.ua>
2018-11-18 01:11:29 +0300

    add radius argument

--- src/component/svg/circle.spec.ts
+++ src/component/svg/circle.spec.ts
@@ -2,6 +2,6 @@ import { Circle } from './circle.js'
 
 describe('svg circle class', () => {
   it('returns element string reprecentation', () => {
-    expect(new Circle().toString()).toContain('<circle')
+    expect(new Circle(50).toString()).toContain('<circle')
   })
 })
--- src/component/svg/circle.ts
+++ src/component/svg/circle.ts
@@ -1,7 +1,11 @@
 import { SVGBaseElement } from './abstract/svg-base-element.js'
 
 export class Circle extends SVGBaseElement {
-  constructor() {
+  constructor(radius: number) {
     super('circle')
+    this.element.setAttribute('r', String(radius))
+
+    this.element.setAttribute('cx', '50%')
+    this.element.setAttribute('cy', '50%')
   }
 }
