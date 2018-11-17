4ac618bd2d0dc120f41e99665fa8024b91e410e1
shamsheev <svr93@i.ua>
2018-11-18 01:16:17 +0300

    add test for size attr check

--- src/component/index.spec.ts
+++ src/component/index.spec.ts
@@ -8,4 +8,15 @@ describe('attribute check', () => {
     expect(element.getAttribute('project')).toBe('https://github.com/svr93/custom-clock')
     done()
   })
+
+  it('checks size attribute', async (done) => {
+    await customElements.whenDefined('custom-clock')
+    const element = document.createElement('custom-clock')
+    document.body.appendChild(element)
+
+    element.setAttribute('size', '200px')
+    expect(Math.round(element.getBoundingClientRect().height)).toBe(200)
+    expect(Math.round(element.getBoundingClientRect().width)).toBe(200)
+    done()
+  })
 })
