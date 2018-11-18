9a00ed51f27576ff869c259cd9a78b904faeab8d
shamsheev <svr93@i.ua>
2018-11-18 19:24:12 +0300

    render circle with black border

--- src/component/svg/circle.ts
+++ src/component/svg/circle.ts
@@ -1,11 +1,14 @@
 import { SVGBaseElement } from './abstract/svg-base-element.js'
 
 export class Circle extends SVGBaseElement {
-  constructor(radius: number) {
+  constructor(radius: number, params: Partial<{ borderColor: string }> = {}) {
     super('circle')
-    this.element.setAttribute('r', String(radius))
+    const BORDER_WIDTH = 1
+    this.element.setAttribute('r', String(radius - BORDER_WIDTH))
 
     this.element.setAttribute('cx', '50%')
     this.element.setAttribute('cy', '50%')
+    this.element.setAttribute('fill', 'transparent')
+    this.element.setAttribute('stroke', params.borderColor || 'black')
   }
 }
