69ecb94625a4441df0b763fb7f9190c7940964f9
shamsheev <svr93@i.ua>
2018-11-24 19:22:03 +0300

    add test for color

--- src/component/svg/line.spec.ts
+++ src/component/svg/line.spec.ts
@@ -7,4 +7,14 @@ describe('svg line class', () => {
       Number(line.element.getAttribute('x1')) - Number(line.element.getAttribute('x2')),
     )).toBe(1)
   })
+
+  it('sets `black` as default color', () => {
+    const line = new Line({ x: 1, y: 2 }, { x: 3, y: 4 })
+    expect(line.element.getAttribute('stroke')).toBe('black')
+  })
+
+  it('sets color if present', () => {
+    const line = new Line({ x: 5, y: 6 }, { x: 7, y: 8 }, { color: 'rgb(100, 200, 300)' })
+    expect(line.element.getAttribute('stroke')).toBe('rgb(100, 200, 300)')
+  })
 })
