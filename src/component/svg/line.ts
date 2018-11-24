0bf3ea470a6146c0b80e6b0d10c97cf1fcc843ec
shamsheev <svr93@i.ua>
2018-11-24 19:17:20 +0300

    add test implementation

--- src/component/svg/line.ts
+++ src/component/svg/line.ts
@@ -5,5 +5,11 @@ type Point = Record<'x' | 'y', number>
 export class Line extends SVGBaseElement {
   constructor(p1: Point, p2: Point, params: Partial<{ color: string }> = {}) {
     super('line')
+
+    this.element.setAttribute('x1', String(p1.x))
+    this.element.setAttribute('x2', String(p2.x))
+
+    this.element.setAttribute('y1', String(p1.y))
+    this.element.setAttribute('y2', String(p2.y))
   }
 }
